21a2035d0e01294c274441ae2c027ab187dc2282
Snoy Kuo <snoy.kuo@twprotech.com>
2021-05-12 12:16:37 +0800

    iOS App done.

---
+++ ios_app/ios_app/SplashViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashViewController.swift
+//  ios_app
+//
+//  Created by Snoy Kuo on 2021/5/12.
+//
+
+import Foundation
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
---
+++ ios_app/ios_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_app
+//
+//  Created by Snoy Kuo on 2021/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
